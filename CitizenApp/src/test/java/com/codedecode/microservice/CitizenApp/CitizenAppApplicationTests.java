ffcf829a412ab431c9c883ffe262ecb1dd64e64d
thejaswini-hegde <thejaswini.hegde@niveussolutions.com>
2022-10-25 01:39:18 +0530

    Test cases for API

--- CitizenApp/src/test/java/com/codedecode/microservice/CitizenApp/CitizenAppApplicationTests.java
+++ CitizenApp/src/test/java/com/codedecode/microservice/CitizenApp/CitizenAppApplicationTests.java
@@ -1,13 +1,35 @@
 package com.codedecode.microservice.CitizenApp;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import com.codedecode.microservice.CitizenApp.Controller.CitizenController;
+import com.codedecode.microservice.CitizenApp.model.Citizen;
+import com.codedecode.microservice.CitizenApp.repository.CitizenRepository;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
 
 @SpringBootTest
 class CitizenAppApplicationTests {
+	
+	@Autowired
+	CitizenRepository citizenRepo;
 
 	@Test
-	void contextLoads() {
-	}
-
+	void testCreate(){
+		
+		Citizen citizen = new Citizen(4,"Rama","A_04");
+		
+		citizen.setId(4);
+		citizen.setName("Rama");
+		citizen.setVaccinationCenterId("A_04");
+		citizenRepo.save(citizen);
+		assertNotNull(citizenRepo.findById(4));
+		}
+	
+	/*void contextLoads() {
+	
+	}*/
+	
+	
 }
